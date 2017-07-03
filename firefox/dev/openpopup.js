4454cf624796b34ea2a830c40d80a4ad9aa855e6
hsroe <jbyxjellybeans@gmail.com>
2017-07-03 13:11:52 -0400

    add things

---
+++ firefox/dev/openpopup.js
@@ -0,0 +1,4 @@
+﻿// i dont know why but the lovely firefox wont run window.open for the pop up
+// and im too annoyed to research otherwise so heres the current solution
+// somebody please fix it thanks
+document.getElementById("whydoihavetodothis").addEventListener("click",function(){window.open('popup.html','_blank')})
---
+++ opera/dev/openpopup.js
@@ -0,0 +1,1 @@
+﻿window.open('popup.html', '_blank');
